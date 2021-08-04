0fd2890133ac4fbb739c2a55d774bb746b1afc60
John McNay <John.McNay@improving.com>
2021-08-04 15:54:19 -0500

    separated models into a file; api skeleton

---
+++ VerifyAddressApi/Controllers/VerifyAddressController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VerifyAddressApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VerifyAddressController : ControllerBase
+    {
+
+        private readonly ILogger<VerifyAddressController> _logger;
+
+        public VerifyAddressController(ILogger<VerifyAddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+
+            return new JsonResult("");
+        }
+    }
+}
---
+++ VerifyAddressPlugin/VerifyAddressModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VerifyAddressPlugin
+{
+
+    [XmlRoot("AddressValidateRequest")]
+    public class AddressValidateRequest
+    {
+        [XmlAttribute]
+        public string USERID = "479IMPRO8097";
+
+        public Address Address { get; set; }
+    }
+
+    [XmlRoot("AddressValidateResponse")]
+    public class AddressValidateResponse
+    {
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip5 { get; set; }
+        public string Zip4 { get; set; }
+        public string ReturnText { get; set; }
+        public Error Error { get; set; }
+    }
+
+    public class Error
+    {
+        public string Number { get; set; }
+        public string Source { get; set; }
+        public string Description { get; set; }
+        public string HelpFile { get; set; }
+        public string HelpContext { get; set; }
+    }
+}
